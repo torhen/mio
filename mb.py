1fc0bb8a0ec965b3e0d506903273e352c7ef663e
torhen <torhen@gmail.com>
2017-04-27 13:10:38 +0200

    Delete mb.py

--- mb.py
+++
@@ -1,5 +0,0 @@
-import os
-def set_color(source_tab, dest_tab,r,g,b):
-    with open('mb.txt','w') as fout:
-        fout.write('set_color\n%s\n%s\n%d\n%d\n%d' % (source_tab, dest_tab,r,g,b))
-    os.system('mb.mbx')
