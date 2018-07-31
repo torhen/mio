f60f56d38fdc7e6df13c6f278e2906552cbeb709
torhen <torhen@gmail.com>
2018-07-31 11:49:20 +0200

    First commit

---
+++ type_drill.py
@@ -0,0 +1,120 @@
+
+from tkinter import *
+from tkinter import messagebox
+import textwrap
+
+
+def init_app(master):
+	canvas = Canvas(master, width=320, height=100)
+	canvas.pack(expand=True, fill='both')
+	return canvas
+
+def read_text(file, letters_per_line):
+	with open(file) as fin:
+		text = fin.read()
+		text = text.replace('„', '"')
+		text = text.replace('“', '"')
+		text = text.replace('–', '-')
+		# keep the excisting linebreaks
+		text = text.split('\n')
+
+		new_text = []
+		for line in text:
+			splitted = textwrap.wrap(line, width= letters_per_line)
+			new_text = new_text + splitted
+
+		new_text = [line + "¬" for line in new_text]
+		return new_text
+
+def draw_text(canvas, text, font, font_size):
+	for line, line_text in enumerate(text):
+		canvas.create_text(5, 5 + 1.5 * font_size * line, text=line_text, anchor='nw', fill='black', font=font)
+
+def clear_cursor():
+	y = 5 + 1.5 * g_font_size * g_cur_line
+	t = g_canvas.create_text(5, y, text=g_text[g_cur_line], anchor='nw', fill='black',  font=g_font)
+	g_canvas.create_rectangle(g_canvas.bbox(t), fill='white', outline='white')
+	t = g_canvas.create_text(5, y, text=g_text[g_cur_line], anchor='nw', fill='black',  font=g_font)
+
+def draw_cursor():
+	y = 5 + 1.5 * g_font_size * g_cur_line
+
+	# just calulate the coordinates of the cursor
+	t = g_canvas.create_text(5, y, text=g_text[g_cur_line][0:g_cur_char], anchor='nw', fill='white',  font=g_font)
+	x0, y0, w0, h0 = g_canvas.bbox(t)
+	t = g_canvas.create_text(5, y, text=g_text[g_cur_line][0:g_cur_char+1], anchor='nw', fill='white',  font=g_font)
+	x1, y1, w1, h1 = g_canvas.bbox(t)
+
+	coord0 = (x0,  y0,  w0 , h0)
+	coord1 = (x1,  y1,  w1 , h1)
+	coord = (w0,   y0, w1, h0)
+
+	g_canvas.create_rectangle(coord, fill='light green', outline='black')
+	t = g_canvas.create_text(5, y, text=g_text[g_cur_line], anchor='nw', fill='black',  font=g_font)
+
+def move_cursor():
+	global g_cur_char, g_cur_line
+
+	clear_cursor()
+
+	g_cur_char += 1
+	if g_cur_char >= len(g_text[g_cur_line]):
+		g_cur_line += 1
+		g_cur_char = 0
+	if g_cur_line >= len(g_text):
+		g_cur_line = 0
+		g_cur_char = 0
+
+	draw_cursor()
+
+def keydown(event):
+
+	c_soll = ord(g_text[g_cur_line][g_cur_char])
+
+
+	try:
+		c_ist = ord(event.char)
+		if c_ist == 10 or c_ist == 13: c_ist = 172
+	except:
+		# ignore shift and other keys
+		return
+
+	
+	if c_ist == c_soll or c_ist==27:
+		move_cursor()
+	else:
+		error_text = f"Expected '{chr(c_soll)}' ({c_soll}) but received '{chr(c_ist)}' ({c_ist})"
+		messagebox.showinfo('Error', error_text)
+		print(error_text)
+
+
+# global settings
+g_font_size = 12
+g_width = 650
+g_height = 500
+g_font_size = 12
+g_font = "Courier"
+g_letters_per_line = 60
+g_cur_line = 0
+g_cur_char = 0
+g_canvas = 0
+g_text = 0
+
+def main():
+	global g_canvas, g_text
+	master = Tk()
+	master.geometry(f'{g_width}x{g_height}')
+	g_canvas = init_app(master)
+
+	g_text = read_text('text.txt', g_letters_per_line)
+	draw_text(g_canvas, g_text, g_font, g_font_size)
+
+	draw_cursor()
+
+
+	master.bind("<KeyPress>", keydown)
+
+	mainloop()
+
+
+main()
